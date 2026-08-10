56212d8cbecd366f9c0a72f98978be28d7d2c858
solamdev <lscheffer740@gmail.com>
2026-08-09 22:27:53 -0300

    Versão Final

---
+++ README.md
@@ -0,0 +1,31 @@
+# 🧠 API de Correção de Texto com IA
+
+Esta é uma API REST desenvolvida em **Node.js** que utiliza a Inteligência Artificial do Google (Gemini) para corrigir textos em português. 
+
+## 🚀 Sobre o Projeto
+
+O objetivo desta API é receber um texto com possíveis erros gramaticais ou de digitação via requisição `POST`, enviar para o modelo de IA do Google, e retornar **apenas** o texto corrigido.
+
+## 🛠️ Tecnologias Utilizadas
+
+- **Node.js**
+- **Express.js**
+- **Google Generative AI (Gemini)**
+- **Dotenv**
+- **CORS**
+
+## ⚙️ Como Rodar Localmente
+
+Para testar a API na sua máquina, siga os passos abaixo:
+
+1. Clone o repositório:
+   ```bash
+   git clone https://github.com/solamdev/api_rest.git
+   
+2. altere token e model pra seus respectivos dados 
+      
+
+## 🤝 Autor
+
+Feito com por Lucas Scheffer
+
--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-
 import { GoogleGenerativeAI } from "@google/generative-ai"
 import { config} from "dotenv";
 import  express, { response }  from "express"
 import  cors  from "cors"
+
+ 
+
 config()
 const app = express()
 const port = 3000
 app.use(express.json());
-app.use(cors())
+app.use(cors({origin: "https://solamdev.github.io/site-de-corrigir-texto/"}));
 const genAI = new GoogleGenerativeAI (process.env.TOKEN)
+
 
 app.post('/chat', async (req, res) => {
   try{
@@ -17,30 +20,37 @@ app.post('/chat', async (req, res) => {
     if (!texto) {
       return res.status(400).json({message: "texto obrigatorio"})
     }
-    console.log(texto)
-    console.log(agr)
+
     const model = genAI.getGenerativeModel({
-      model: "gemini-2.0-flash-lite",
-     systemInstruction: `Sua única e exclusiva função é corrigir textos em português. 
-     Você receberá um texto com erros e deve retornar APENAS o texto corrigido de forma ${agr}. 
-     Não adicione absolutamente nada antes ou depois do texto corrigido. Não use saudações, 
-     explicações, comentários ou formatação Markdown`
+      model: process.env.MODEL,
+      config: {
+        
+        thinkingConfig: {
+          thinkingBudget: 0
+        }
+        
+      },
+      
+      systemInstruction: `Sua única e exclusiva função é corrigir textos em português. 
+      Você receberá um texto com erros e deve retornar APENAS o texto corrigido de forma ${agr}. 
+      Não adicione absolutamente nada antes ou depois do texto corrigido. Não use saudações, 
+      explicações, comentários ou formatação Markdown`
     })
     const result = await model.generateContent(texto)
     
     const response =  result.response
     const respottext = response.text()
     res.json({msg: respottext})
-    console.log(respottext)
+ 
   }
   catch (error){
+    
     console.error("erro no endpoint", error)
     res.status(500).json({error: "erro no lado do servidor"})
     }
   })
   
   app.listen(port ,() =>{
-    console.log(`Servidor rodando em http://localhost:${port}`)
+   
   })
 
-  
